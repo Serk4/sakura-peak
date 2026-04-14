65fb0a183982e39d056ccf92026502bb61adb91e
David Serkland <12652101+Serk4@users.noreply.github.com>
2026-04-14 12:36:57 -0400

    Restructure: flatten project to root, fix Tailwind v3, fix postcss/tailwind config paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        sakura: {
-          light: '#FFB7C5',
-          DEFAULT: '#FF8FAB',
-          dark: '#FF6B8A',
-        }
-      }
-    },
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				sakura: {
+					light: '#FFB7C5',
+					DEFAULT: '#FF8FAB',
+					dark: '#FF6B8A',
+				},
+			},
+		},
+	},
+	plugins: [],
 }
